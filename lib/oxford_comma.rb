cda4aec16081bb07f15f90d598033b0a59ca12ad
Sara A <calisaraalhindi@gmail.com>
2020-08-07 17:16:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ if array.length == 1
   elsif array.length == 2
   return array.join(" and ")
   elsif array.length >= 3
-  array[-1] = "and #{array[-1]}"
+  array[1] = "and #{array[1]}"
   return array.join(", ")
 end
 end
